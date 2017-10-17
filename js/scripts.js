22526a11303971dd3c8ab8d63db2286d73627d13
Ginger Lee Kretchmer and Michael Pinaud <michael@>
2017-10-17 16:19:02 -0700

    Add function for dog button to show a list with response

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 $(function() {
   $("button.cat").click(function() {
     $("ul.cat").prepend("<li>Meow</li>");
+    $("ul.dog").prepend("<li>What?</li>");
   });
+
+  $("button.dog").click(function() {
+    $("ul.dog").prepend("<li>Bark</li>");
+    $("ul.cat").prepend("<li>I'm so over you!</li>");
+  });
+
 });
